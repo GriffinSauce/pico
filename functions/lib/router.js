b58a6cdc472c3ba2b1f89cdcfa56e6e2757a31f4
Joris Griffioen <jorisjgriffioen@gmail.com>
2019-09-23 15:02:34 +0200

    Syntax

--- functions/lib/router.js
+++ functions/lib/router.js
@@ -19,9 +19,9 @@
  */
 const pathToRegexp = require('path-to-regexp');
 
-const GET = "GET";
-const POST = "POST";
-const ANY = "ANY";
+const GET = 'GET';
+const POST = 'POST';
+const ANY = 'ANY';
 
 // Return a clean API path
 const cleanPath = path => path.replace('/.netlify/functions', '');
@@ -41,19 +41,19 @@ const parseParams = (match, keys) =>
   }, {});
 
 // Match the path and return url params - see path-to-regexp
-const matchPath = ({ event, path}) => {
+const matchPath = ({ event, path }) => {
   const keys = [];
   const regexp = pathToRegexp(convertWildcard(path), keys);
   const match = regexp.exec(getPath(event));
   return {
     match,
     params: match ? parseParams(match, keys) : {},
-  }
-}
+  };
+};
 
 const createRouter = () => {
   const handlers = [];
-  
+
   const createHandler = method => {
     return (path, handler) => {
       handlers.push({
@@ -61,22 +61,22 @@ const createRouter = () => {
         path,
         handler,
       });
-    },
-  }
-  
+    };
+  };
+
   return {
-    get: createHandler(GET);
-    
-    post: createHandler(POST);
-    
-    any: createHandler(ANY);
+    get: createHandler(GET),
+
+    post: createHandler(POST),
+
+    any: createHandler(ANY),
 
     handle: (event, context) => {
       let res;
       handlers.some(({ method, path, handler }) => {
         if (method !== ANY && event.httpMethod !== method) return;
-        
-        const { match, params } = getMatch({ event, path })
+
+        const { match, params } = getMatch({ event, path });
         if (!match) return;
 
         res = handler({ params, event, context });
