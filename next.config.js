e9dfab0cdd82802442a37ad2de6bf5edcf6783a8
Joris Griffioen <jorisjgriffioen@gmail.com>
2019-09-24 23:21:29 +0200

    Aaaaah

--- next.config.js
+++ next.config.js
@@ -2,10 +2,6 @@ const path = require('path');
 const glob = require('glob');
 
 module.exports = {
-  env: {
-    MONGO_PASSWORD: process.env.MONGO_PASSWORD,
-    MONGO_USER: process.env.MONGO_USER,
-  },
   webpack: (config, { dev }) => {
     config.module.rules.push({
       test: /\.test.js$/,
