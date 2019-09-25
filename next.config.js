19090244b85a504b048902dc511d3c8a4d358a25
root <root@DESKTOP-F5EFCTJ.localdomain>
2019-09-25 08:57:29 +0200

    Simpler sentry?

--- next.config.js
+++ next.config.js
@@ -3,7 +3,11 @@ const glob = require('glob');
 const withSourceMaps = require('@zeit/next-source-maps')();
 
 module.exports = withSourceMaps({
-  webpack: (config, { dev }) => {
+  webpack: (config, options) => {
+    if (!options.isServer) {
+      config.resolve.alias['@sentry/node'] = '@sentry/browser';
+    }
+
     config.module.rules.push({
       test: /\.test.js$/,
       loader: 'ignore-loader',
