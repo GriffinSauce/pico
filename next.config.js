3660ca052b5fbbad9bd3a2d892fddde778a0b7e1
root <root@DESKTOP-F5EFCTJ.localdomain>
2019-09-25 08:49:31 +0200

    Add Sentry

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const glob = require('glob');
+const withSourceMaps = require('@zeit/next-source-maps')();
 
-module.exports = {
+module.exports = withSourceMaps({
   webpack: (config, { dev }) => {
     config.module.rules.push({
       test: /\.test.js$/,
@@ -9,4 +10,4 @@ module.exports = {
     });
     return config;
   },
-};
+});
---
+++ pages/_app.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import App, { Container } from 'next/app';
+import * as Sentry from '@sentry/browser';
+
+Sentry.init({
+  dsn: 'https://225c0c41e53e4852a221b260bd070b27@sentry.io/1760702',
+});
+
+class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return { pageProps };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    Sentry.withScope(scope => {
+      Object.keys(errorInfo).forEach(key => {
+        scope.setExtra(key, errorInfo[key]);
+      });
+
+      Sentry.captureException(error);
+    });
+
+    super.componentDidCatch(error, errorInfo);
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <Container>
+        <Component {...pageProps} />
+      </Container>
+    );
+  }
+}
+
+export default MyApp;
