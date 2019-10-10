d6b8c8939cc581acffabeb14179402a2815fba33
Joris Griffioen <jorisjgriffioen@gmail.com>
2019-10-09 19:35:21 -1000

    Some admin UI to review albums

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,6 +1,8 @@
 import react, { useState } from 'react';
 import Router from 'next/router';
+import Link from 'next/link';
 import { motion, AnimatePresence } from 'framer-motion';
+import Layout from '~/components/Layout';
 import Logo from '~/components/Logo';
 import createApi from '~/lib/createApi';
 import hostFromReq from '~/lib/hostFromReq';
@@ -8,17 +10,27 @@ import useInternetStatus from '~/lib/useInternetStatus';
 
 function AdminHome({ host, requests }) {
   return (
-    <>
+    <Layout>
       <header>
         <Logo />
       </header>
 
+      <h1>Albums</h1>
+
       <ul>
         {requests.map(request => (
-          <li>
+          <li key={request.id}>
             <Link href={request.uri}>
-              <a>{request.title}</a>
+              <a>
+                <h2>{request.title}</h2>
+              </a>
             </Link>
+            <div className="album-id">{request.id}</div>
+            <div>
+              {request.media.map(item => (
+                <img src={`${item.url}-/scale_crop/150x150/center/`} />
+              ))}
+            </div>
           </li>
         ))}
       </ul>
@@ -27,8 +39,43 @@ function AdminHome({ host, requests }) {
         header {
           margin: 50px 0;
         }
+
+        ul {
+          list-style: none;
+          margin: 0;
+          padding: 0;
+        }
+
+        li {
+          margin: 0 0 15px 0;
+          padding: 10px;
+          text-align: left;
+          border-radius: 4px;
+          box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.35);
+        }
+
+        li a {
+          text-decoration: none;
+        }
+
+        li h2 {
+          margin: 0 0 5px 0;
+          color: #000;
+        }
+
+        li .album-id {
+          margin: 0 0 5px 0;
+          font-size: 12px;
+          color: #aaa;
+        }
+
+        li img {
+          margin: 0 5px 5px 0;
+          width: 75px;
+          border-radius: 4px;
+        }
       `}</style>
-    </>
+    </Layout>
   );
 }
 
--- pages/api/requests/index.js
+++ pages/api/requests/index.js
@@ -4,7 +4,7 @@ module.exports = async (req, res) => {
   if (req.method === 'GET') {
     let requests;
     try {
-      requests = await Request.find().lean();
+      requests = await Request.find();
     } catch (error) {
       console.error('Error finding requests', error);
       return res.status(500).send({
@@ -12,6 +12,19 @@ module.exports = async (req, res) => {
       });
     }
     return res.status(200).json({ requests });
+  }
+
+  if (req.method === 'POST') {
+    const request = new Request(req.body);
+    try {
+      await request.save();
+    } catch (error) {
+      console.error('Error creating request', error);
+      return res.status(500).send({
+        error,
+      });
+    }
+    return res.status(200).json({ request });
   }
 
   return res.status({ status: 404 }).send();
