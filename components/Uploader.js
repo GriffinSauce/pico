81099a060036e0233fdc908dc789586ffb1da79c
GriffinSauce <jorisjgriffioen@gmail.com>
2020-09-15 22:50:47 +0200

    Rename to Uploader and remove Uploadcare - baiiii

--- components/Uploader.js
+++ components/Uploader.js
@@ -1,70 +1,133 @@
-import { useRef } from 'react';
-import { Widget } from '@uploadcare/react-widget';
+import { useState, useEffect, useRef } from 'react';
 import { MdCameraAlt } from 'react-icons/md';
+import { FiX } from 'react-icons/fi';
+import { storage } from '../lib/firebase';
+import ProgressCircle from './ProgressCircle';
 import Button from './Button';
 
-if (process.browser) {
-  window.UPLOADCARE_LOCALE_TRANSLATIONS = {
-    buttons: {
-      cancel: 'Cancel',
-      remove: 'Remove',
-      choose: {
-        files: {
-          one: 'Add a picture',
-          other: 'Add a pictures',
-        },
-        images: {
-          one: 'Add a picture',
-          other: 'Add pictures',
-        },
-      },
-    },
-  };
-}
+const sum = (arr) => arr.reduce((newSum, num) => newSum + (num || 0), 0);
 
-const fileInfoToMedia = (fileInfo) => ({
-  url: fileInfo.cdnUrl,
-  filename: fileInfo.name,
-  type: fileInfo.mimeType,
+const fileToMedia = (file) => ({
+  url: file.url,
+  filename: file.name,
+  type: file.type,
 });
 
-const Uploader = ({ onChange }) => {
-  const widgetApi = useRef();
+const ImageUpload = ({ onChange }) => {
+  const inputRef = useRef(null);
+  const [files, setFiles] = useState([]);
+
+  // Compute avg. total progress
+  const progress = Math.round(
+    sum(files.map((file) => file.progress)) || 0 / files.length,
+  );
+
+  const reset = () => {
+    // Reset
+    setFiles([]);
+    inputRef.current.value = '';
+  };
+
+  const onUploadsComplete = () => {
+    onChange(files.map(fileToMedia));
+    reset();
+  };
+
+  const onCancel = () => {
+    files.map((file) => file.task?.cancel());
+    reset();
+  };
+
+  const areUploadsComplete =
+    files.length && files.every((file) => file.isComplete);
+  useEffect(() => {
+    if (areUploadsComplete) onUploadsComplete();
+  }, [areUploadsComplete]);
+
+  const handleChange = (event) => {
+    if (!event.target.files.length) return;
+
+    const filesArr = Array.from(event.target.files);
+
+    setFiles(filesArr);
+
+    filesArr.forEach((file, index) => {
+      const onProgress = (snapshot) => {
+        // progress
+        const fileProgress = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100,
+        );
+
+        // TODO: ew, use Immer
+        setFiles((oldFiles) => {
+          const updatedFiles = [...oldFiles];
+          updatedFiles[index].progress = fileProgress;
+          return updatedFiles;
+        });
+      };
+
+      const onError = (error) => {
+        // error
+        console.log(error);
+      };
+
+      const onComplete = async () => {
+        const firebaseObject = storage.ref('images').child(file.name);
+        const url = await firebaseObject.getDownloadURL();
+
+        // TODO: ew, use Immer
+        setFiles((oldFiles) => {
+          const updatedFiles = [...oldFiles];
+          updatedFiles[index].url = url;
+          updatedFiles[index].isComplete = true;
+          return updatedFiles;
+        });
+      };
+
+      const uploadTask = storage.ref(`images/${file.name}`).put(file);
+      uploadTask.on('state_changed', onProgress, onError, onComplete);
+
+      setFiles((oldFiles) => {
+        const updatedFiles = [...oldFiles];
+        updatedFiles[index].task = uploadTask;
+        return updatedFiles;
+      });
+    });
+  };
+
+  const onSelectFiles = () => {
+    inputRef.current.click();
+  };
 
   return (
     <>
-      <Button onClick={() => widgetApi.current.openDialog()}>
-        <MdCameraAlt className="-ml-1 text-2xl" />
-        <span>Add pictures</span>
-      </Button>
+      <input
+        className="hidden"
+        type="file"
+        multiple
+        accept="image/*,video/*"
+        ref={inputRef}
+        onChange={handleChange}
+      />
 
-      <div>
-        {process.browser ? (
-          <Widget
-            ref={widgetApi}
-            publicKey="3ee62abfc85924be3d0e"
-            multiple
-            systemDialog
-            imagesOnly
-            onFileSelect={async (filesInfo) => {
-              const files = await Promise.all(filesInfo.files());
-              onChange(files.map(fileInfoToMedia));
-            }}
-          />
-        ) : null}
-      </div>
-
-      <style jsx global>{`
-        .uploadcare--widget {
-          display: block;
-          width: 100%;
-        }
-        .uploadcare--widget__button_type_open {
-          display: none;
-        }
-      `}</style>
+      {files.length ? (
+        <div className="flex items-center justify-center space-x-3">
+          <div className="flex-shrink-0 w-10 h-10">
+            <ProgressCircle percentage={progress} />
+          </div>
+          <Button size="sm" onClick={onCancel}>
+            <FiX className="-ml-1 text-xl" />
+            <span>cancel</span>
+          </Button>
+        </div>
+      ) : (
+        <Button onClick={onSelectFiles}>
+          <MdCameraAlt className="-ml-1 text-2xl" />
+          <span>Add pictures</span>
+        </Button>
+      )}
     </>
   );
 };
 
-export default Uploader;
+export default ImageUpload;
--- pages/a/[slug].js
+++ pages/a/[slug].js
@@ -5,7 +5,7 @@ import createApi from '../../lib/createApi';
 import hostFromReq from '../../lib/hostFromReq';
 import Layout from '../../components/Layout';
 import Logo from '../../components/Logo';
-import Uploader from '../../components/FirebaseUploader';
+import Uploader from '../../components/Uploader';
 import Downloader from '../../components/Downloader';
 import Button from '../../components/Button';
 import AlbumLink from '../../components/AlbumLink';
